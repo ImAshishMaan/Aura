25ff2d8114ef38d1264b26bf36cfcd6c2bcae4a2
Ashish <ashishmaan22@gmail.com>
2024-04-27 17:37:57 +0530

    Ability System component and attribute Set classes

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,8 @@
+#include "Player/AuraPlayerState.h"
+
+
+AAuraPlayerState::AAuraPlayerState() {
+	NetUpdateFrequency = 100.f;
+	
+	
+}
---
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AuraAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAttributeSet : public UAttributeSet {
+	GENERATED_BODY()
+	
+};
