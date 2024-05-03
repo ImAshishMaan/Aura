f59c8b4837f94d2fe6a25afd7c7c9bd381d33248
Ashish <ashishmaan22@gmail.com>
2024-05-03 15:46:38 +0530

    Curce table for scalable heal and mana floats

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -66,4 +66,6 @@ protected:
 
 	TMap<FActiveGameplayEffectHandle, UAbilitySystemComponent*> ActiveEffectHandles;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	float ActorLevel = 1.f;
 };
